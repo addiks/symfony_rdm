f9d3d6c4085ad508bc43e556b123615d0f665586
Sarah Mohrmann <sarah.mohrmann@brille24.de>
2021-01-27 12:29:47 +0100

    Update TreeBuilder for Symfony 4.4, drop support for Symfony 4.1 and below.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('addiks_symfony_rdm');
+        $treeBuilder = new TreeBuilder('addiks_symfony_rdm');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
